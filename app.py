1749ea98acd35f6d8ebe446038da1ac5c7ac344d
Alex-Chava <alexchav@gmail.com>
2025-03-13 14:18:53 +0300

    Final

--- app.py
+++ app.py
@@ -214,4 +214,4 @@ def scan_coils():
             instrument.serial.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Включите debug для отладки
